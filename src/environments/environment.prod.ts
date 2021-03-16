c712666d0ee613e1e6f87db0618c4906e8b1d95e
Munoz Daniel <dmunoz@micracel.com.ar>
2021-03-16 16:36:49 -0300

    environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,29 @@
 export const environment = {
-    production: true,
-    hmr       : false,
-    apiURI    : 'http://localhost:8083/api/',
+  production: true,
+  hmr: false,
+  apiURI: 'http://165.232.137.17:8083/api/',
+  BASE_URI: () => {
+    const protocol = window.location.protocol;
+    const host = window.location.host;
+    return `${protocol}//${host}`;
+  },
+  auth: {
+    domain: 'escuela-mean-stack.us.auth0.com',
+    scope: 'openid profile email',
+    audience: 'http://165.232.137.17:8083/api/',
+    clientId: 'FWiKlj4EGLUIYq0l8dlwk3SMuHMa46IR',
+    // redirectUri: window.location.origin,
+    redirectUri: 'http://165.232.137.17:4200/callback',
+    // The AuthHttpInterceptor configuration
+    httpInterceptor: {
+      allowedList: [
+        // Attach access tokens to any calls to '/api' (exact match)
+        '/api',
 
+        // Attach access tokens to any calls that start with '/api/'
+        '/api/*',
+      ],
+    },
+    namespace: 'http://myapp.com/roles',
+  },
 };
