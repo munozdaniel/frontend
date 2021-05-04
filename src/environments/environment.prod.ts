5008f9513d22e23d67e5d6689ae0756767e3bd26
Munoz Daniel <dmunoz@micracel.com.ar>
2021-05-04 15:53:38 -0300

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   hmr: false,
-  apiURI: 'http://app.cet30.edu.ar:8081/api/',
+  apiURI: 'http://localhost:8081/api/',
   BASE_URI: () => {
     const protocol = window.location.protocol;
     const host = window.location.host;
