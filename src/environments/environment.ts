c3ddcbbfbcfe3d6cb15834ebb9b6b8b1a565e1a7
Munoz Daniel <dmunoz@micracel.com.ar>
2021-05-07 19:11:57 -0300

    environmet 8081

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,10 @@
 export const environment = {
-    production: false,
-    hmr: false,
-    apiURI: "http://localhost:8083/api/",
-    BASE_URI: () => {
-        const protocol = window.location.protocol;
-        const host = window.location.host;
-        return `${protocol}//${host}`;
-    },
-   
+  production: false,
+  hmr: false,
+  apiURI: 'http://localhost:8081/api/',
+  BASE_URI: () => {
+    const protocol = window.location.protocol;
+    const host = window.location.host;
+    return `${protocol}//${host}`;
+  },
 };
