1728771a5d888d7531978dbd89c4d165d24bc5b5
Munoz Daniel <dmunoz@micracel.com.ar>
2021-06-27 12:09:52 -0300

    logout unauthorized

--- src/app/core/interceptors/unauthorized.interceptor.ts
+++ src/app/core/interceptors/unauthorized.interceptor.ts
@@ -1,31 +1,22 @@
-import { Injectable } from "@angular/core";
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-} from "@angular/common/http";
-import { Observable, throwError } from "rxjs";
-import { catchError } from "rxjs/operators";
-import { Router } from "@angular/router";
-import { AuthService } from "../auth/auth.service";
-import { environment } from "environments/environment";
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { environment } from 'environments/environment';
 
 @Injectable()
 export class UnauthorizedInterceptor implements HttpInterceptor {
   constructor(private authService: AuthService, private router: Router) {}
 
-  intercept(
-    request: HttpRequest<unknown>,
-    next: HttpHandler
-  ): Observable<HttpEvent<unknown>> {
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((err) => {
         if (err.status === 401) {
           this.authService.clearLocalStorage();
-          this.router.navigate(["/auth/iniciar-sesion"], {
-            queryParams: { returnUrl: this.router.routerState.snapshot.url },
-          });
+          this.authService.logout();
+          this.router.navigate(['/auth/iniciar-sesion']);
         }
 
         if (!environment.production) {
